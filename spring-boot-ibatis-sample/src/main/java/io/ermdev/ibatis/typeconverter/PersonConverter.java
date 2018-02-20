3817788768b31d3da8b5ec5c01f495d684f9f655
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-20 08:55:35 +0800

    Update

--- spring-boot-ibatis-sample/src/main/java/io/ermdev/ibatis/typeconverter/PersonConverter.java
+++ spring-boot-ibatis-sample/src/main/java/io/ermdev/ibatis/typeconverter/PersonConverter.java
@@ -2,20 +2,15 @@ package io.ermdev.ibatis.typeconverter;
 
 import io.ermdev.ibatis.data.person.Person;
 import io.ermdev.ibatis.data.person.PersonRepository;
-import mapfierj.TypeConverter;
 import mapfierj.TypeConverterAdapter;
 import mapfierj.TypeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@TypeConverter
 public class PersonConverter extends TypeConverterAdapter<Long, Person> {
 
     private PersonRepository personRepository;
-
-    public PersonConverter() {
-    }
 
     @Autowired
     public PersonConverter(PersonRepository personRepository) {
@@ -27,12 +22,16 @@ public class PersonConverter extends TypeConverterAdapter<Long, Person> {
         try {
             return personRepository.findById(ownerId);
         } catch (NullPointerException e) {
-            return null;
+            throw new TypeException();
         }
     }
 
     @Override
     public Long convertFrom(Person person) throws TypeException {
-        return person.getId();
+        try {
+            return person.getId();
+        } catch (Exception e) {
+            throw new TypeException();
+        }
     }
 }
